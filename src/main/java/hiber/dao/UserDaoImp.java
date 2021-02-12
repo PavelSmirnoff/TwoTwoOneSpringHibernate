6d3a872723142307323363322bb9b75630c70254
Pavel Smirnov <smirnov@0x55.ru>
2021-02-12 23:50:33 +0300

    Car add for User

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -1,5 +1,6 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,6 @@ public class UserDaoImp implements UserDao {
 
    @Autowired
    private SessionFactory sessionFactory;
-
    @Override
    public void add(User user) {
       sessionFactory.getCurrentSession().save(user);
